806afd247637971a1745486f368fbb6ece293dde
TheAlexGET <sucevanalex06@gmail.com>
2022-12-08 16:48:25 +0100

    best architecture

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 /*
 Создайте проект с файлами HTML, CSS и Javascript и настройте репозиторий Git. (done)
 
-Вы представите игровую доску как массив внутри объекта Gameboard. Информация о игроках также будет храниться в объектах... и вы, вероятно создадите объект, который 
+Вы представите игровую доску как массив внутри объекта Game. Информация о игроках также будет храниться в объектах... и вы, вероятно создадите объект, который 
 будет контролировать ход самой игры.
 
 Ваша главная задача - иметь как можно меньше глобального кода. Попробуйте спрятать все внутри модуля или фабрики. Закономерность: если вам нужно что-то в ЕДИНСТВЕННОМ экземпляре 
-(gameBoard, displayController), используйте модуль. Если вам нужно что-то многократно (игроки к примеру!), созавайте их с помощью фабрики.
+(game, displayController), используйте модуль. Если вам нужно что-то многократно (игроки к примеру!), созавайте их с помощью фабрики.
 Добавьте HTML и напишите функцию JavaScript, которая будет отображать содержимое массива игровой доски на веб-страницу (сейчас вы можете просто вручную заполнить массив X и O).
 
 Создайте функции, позволяющие игрокам добавлять отметку в определенное место на доске (привязать к DOM). Не забудьте сделать проверку, которая мешает игрокам ставить маркер в местах,
@@ -19,118 +19,146 @@
 е игрокам ввести свои имена, добавьте кнопку для запуска / перезапуска игры и элемент отображения, который поздравляет победившего игрока!
 */
 
-//creating gameBoard
+//creating game
 window.onload = function () {
-  let player1 = null
-  let player2 = null
-  const Gameboard = {
+  let player1 = null;
+  let player2 = null;
+  let Game = {
     board: function () {
       return document.querySelectorAll("button");
     },
   };
 
   //creating Player
-  let i = 1;
-  const Player = (playerName) => {
-     const playerSign1 = 'X';
-     const playerSign2 = 'O'
-      return {playerName, playerSign1, playerSign2};
-    }
-
+  const Player = (playerName, playerSign) => {
+    return { playerName, playerSign };
+  };
   //future // init players when player click the button
-  document.querySelector('#newPlayer').onclick = function(){
-    player1 = Player(prompt("Player1 insert your name:"));
-    player2 = Player(prompt("Player2 insert your name:"))
-    }
+  document.querySelector("#newPlayer").onclick = function () {
+    player1 = Player(prompt("Player1 insert your name:"), "X");
+    player2 = Player(prompt("Player2 insert your name:"), "O");
+  };
   let gameStatus = {
     turn: 1,
   };
 
   //render function
   const render = (() => {
-    for (i = 0; i < Gameboard.board().length; i++) {
+    for (i = 0; i < Game.board().length; i++) {
       //done
       let num = i;
-      Gameboard.board()[i].onclick = function () {
-        if(player1 == null){
-          return alert('Create new Players please ;)')
+      Game.board()[i].onclick = function () {
+        let board = [
+          [
+            Game.board()[0].textContent,
+            Game.board()[1].textContent,
+            Game.board()[2].textContent,
+          ],
+          [
+            Game.board()[3].textContent,
+            Game.board()[4].textContent,
+            Game.board()[5].textContent,
+          ],
+          [
+            Game.board()[6].textContent,
+            Game.board()[7].textContent,
+            Game.board()[8].textContent,
+          ],
+        ];
+        if (player1 == null) {
+          return alert("Create new Players please ;)");
         }
         if (gameStatus.turn === 1) {
-          if (Gameboard.board()[num].textContent != "") {
+          if (Game.board()[num].textContent != "") {
             //check function if sign isnt added yet
             return;
           }
-          Gameboard.board()[num].textContent = player1.playerSign1;
+          Game.board()[num].textContent = player1.playerSign;
           gameStatus.turn -= 1;
         } else if (gameStatus.turn === 0) {
-          if (Gameboard.board()[num].textContent != "") {
+          if (Game.board()[num].textContent != "") {
             //check function if sign isnt added yet
             return;
           }
-          Gameboard.board()[num].textContent = player2.playerSign2;
+          Game.board()[num].textContent = player2.playerSign;
           gameStatus.turn += 1;
         }
-        let matches = [ //base of possible win matches
-          //0, 1, 2
-          `${Gameboard.board()[0].textContent}${
-            Gameboard.board()[1].textContent
-          }${Gameboard.board()[2].textContent}`,
-          //0, 3, 6
-          `${Gameboard.board()[0].textContent}${
-            Gameboard.board()[3].textContent
-          }${Gameboard.board()[6].textContent}`,
-          //0, 4, 8
-          `${Gameboard.board()[0].textContent}${
-            Gameboard.board()[4].textContent
-          }${Gameboard.board()[8].textContent}`,
-          //3, 4, 5
-          `${Gameboard.board()[3].textContent}${
-            Gameboard.board()[4].textContent
-          }${Gameboard.board()[5].textContent}`,
-          //6, 7, 8
-          `${Gameboard.board()[6].textContent}${
-            Gameboard.board()[7].textContent
-          }${Gameboard.board()[8].textContent}`,
-          //1, 4, 7
-          `${Gameboard.board()[1].textContent}${
-            Gameboard.board()[4].textContent
-          }${Gameboard.board()[7].textContent}`,
-          //2, 4, 6
-          `${Gameboard.board()[2].textContent}${
-            Gameboard.board()[4].textContent
-          }${Gameboard.board()[6].textContent}`,
-        ];
-        if (
-          matches[0] == "XXX" ||
-          matches[0] == "OOO" ||
-          matches[1] == "XXX" ||
-          matches[1] == "OOO" ||
-          matches[2] == "XXX" ||
-          matches[2] == "OOO" ||
-          matches[3] == "XXX" ||
-          matches[3] == "OOO" ||
-          matches[4] == "XXX" ||
-          matches[4] == "OOO" ||
-          matches[5] == "XXX" ||
-          matches[5] == "OOO" ||
-          matches[6] == "XXX" ||
-          matches[6] == "OOO" ||
-          matches[7] == "XXX" ||
-          matches[7] == "OOO" ||
-          matches[8] == "XXX" ||
-          matches[8] == "OOO"
-        ) {
-          Gameboard.board().forEach((key) => {
-            return (key.disabled = true);
-          });
-          if (gameStatus.turn == 0) {
+        for(i = 0; i < 7; i += 3) {
+          if (
+            Game.board()[i].textContent == Game.board()[i + 1].textContent &&
+            Game.board()[i + 1].textContent == Game.board()[i + 2].textContent
+          ) {
+            if (Game.board()[i].textContent == "X") {
+              document.querySelector(
+                "h1"
+              ).textContent = `A castigat: ${player1.playerName}!!!`;
+              Game.board().forEach((key) => {
+                return (key.disabled = true);
+              });
+            } else if (Game.board()[i].textContent == "O") {
+              document.querySelector(
+                "h1"
+              ).textContent = `A castigat: ${player2.playerName}!!!`;
+              Game.board().forEach((key) => {
+                return (key.disabled = true);
+              });
+            }
+          }
+        }
+        for (i = 0; i < 3; i++) {
+          if (
+            Game.board()[i].textContent == Game.board()[i + 3].textContent &&
+            Game.board()[i + 3].textContent == Game.board()[i + 6].textContent
+          ) {
+            if (Game.board()[i].textContent == "X") {
+              document.querySelector(
+                "h1"
+              ).textContent = `A castigat: ${player1.playerName}!!!`;
+              Game.board().forEach((key) => {
+                return (key.disabled = true);
+              });
+            } else if (Game.board()[i].textContent == "O") {
+              document.querySelector(
+                "h1"
+              ).textContent = `A castigat: ${player2.playerName}!!!`;
+              Game.board().forEach((key) => {
+                return (key.disabled = true);
+              });
+            }
+          }
+        }
+        if(Game.board()[0].textContent == Game.board()[4].textContent && Game.board()[4].textContent == Game.board()[8].textContent){
+          if (Game.board()[0].textContent == "X") {
             document.querySelector(
-              "#congrat"
-            ).textContent = `A castigat: ${player1.playerName}!`;
-          } else if (gameStatus.turn == 1) {
+              "h1"
+            ).textContent = `A castigat: ${player1.playerName}!!!`;
+            Game.board().forEach((key) => {
+              return (key.disabled = true);
+            });
+          } else if (Game.board()[0].textContent == "O") {
             document.querySelector(
-              "#congrat"
-            ).textContent = `A castigat: ${player2.playerName}!`;
+              "h1"
+            ).textContent = `A castigat: ${player2.playerName}!!!`;
+            Game.board().forEach((key) => {
+              return (key.disabled = true);
+            });
+          }
+        }
+        if(Game.board()[2].textContent == Game.board()[4].textContent && Game.board()[4].textContent == Game.board()[6].textContent){
+          if (Game.board()[2].textContent == "X") {
+            document.querySelector(
+              "h1"
+            ).textContent = `A castigat: ${player1.playerName}!!!`;
+            Game.board().forEach((key) => {
+              return (key.disabled = true);
+            });
+          } else if (Game.board()[2].textContent == "O") {
+            document.querySelector(
+              "h1"
+            ).textContent = `A castigat: ${player2.playerName}!!!`;
+            Game.board().forEach((key) => {
+              return (key.disabled = true);
+            });
           }
         }
       };
