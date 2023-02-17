78f8f0e8b8225a7c344d0a68360acaa5a4ea2074
TheAlexGET <sucevanalex06@gmail.com>
2023-02-17 16:47:58 +0100

    phone adaptive

--- test.js
+++ test.js
@@ -1,14 +1,22 @@
-class User {
-    constructor(firstName, lastName) {
-      this.firstName = firstName;
-      this.lastName = lastName;
-    }
-  
-    static createGuest() {
-      return new User("Гость", "Сайта");
-    }
-  };
-  
-  let user = new User('Alex', 'Sucevan');
-  
-  console.log( user.firstName ); // Гость
+'use strict';
+
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+
+  walk() {
+    alert("I walk: " + this.name);
+  }
+}
+
+class Rabbit extends Animal {
+  walk() {
+    super.walk();
+    alert("...and jump!");
+  }
+}
+
+new Rabbit("Вася").walk();
+// I walk: Вася
+// and jump!
